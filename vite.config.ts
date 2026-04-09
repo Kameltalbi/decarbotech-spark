2047825983a4369e2472ac7a048e06727b15c4b3
Kamel Talbi <kameltalbi.tn@gmail.com>
2026-04-09 16:40:05 +0100

    fix: syntax error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,4 @@ export default defineConfig({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-}));
+});
